a6c35fa8aed4520b3d12e7361dc0395b9c95cd4c
Alexandr Voronsky <voronsky@ukr.net>
2015-09-06 01:11:00 +0300

    some lyouts

---
+++ app/src/main/java/com/smartcity/kyivdeafservice/app/adapters/NewsAdapter.java
@@ -0,0 +1,26 @@
+package com.smartcity.kyivdeafservice.app.adapters;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.smartcity.kyivdeafservice.app.R;
+
+/**
+ * Created by voronsky on 06.09.15.
+ */
+public class NewsAdapter {
+
+//    private void initViews() {
+//        private ImageView imageNews;
+//        private TextView titleNews;
+//        private TextView dateNews;
+//        private TextView timeNews;
+//
+//        imageNews = (ImageView) findViewById(R.id.imageNews);
+//        titleNews = (TextView) findViewById(R.id.titleNews);
+//        dateNews = (TextView) findViewById(R.id.dateNews);
+//        timeNews = (TextView) findViewById(R.id.timeNews);
+//    }
+}
+
---
+++ app/src/main/java/com/smartcity/kyivdeafservice/app/fragments/MapFragment.java
@@ -0,0 +1,7 @@
+package com.smartcity.kyivdeafservice.app.fragments;
+
+/**
+ * Created by voronsky on 05.09.15.
+ */
+public class MapFragment {
+}
